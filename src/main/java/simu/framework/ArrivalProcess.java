9a347f5f8db8bef050381741bfd1e39a190a6890
Jukka <savela.j@gmail.com>
2024-05-06 14:18:14 +0300

    framework docs in work

--- src/main/java/simu/framework/ArrivalProcess.java
+++ src/main/java/simu/framework/ArrivalProcess.java
@@ -3,6 +3,10 @@ package simu.framework;
 import eduni.distributions.ContinuousGenerator;
 import simu.model.EventType;
 
+/**
+ * This class represents the arrival process in the simulation.
+ * It provides methods to generate the next event in the process.
+ */
 public class ArrivalProcess {
 	private ContinuousGenerator generator;
 	private EventList eventList;
@@ -10,6 +14,13 @@ public class ArrivalProcess {
 	private int customerCount = 0;
 	private final int maxCustomers;
 
+	/**
+	 * Constructs a new ArrivalProcess with the given generator, event list, event type, and maximum number of customers.
+	 * @param generator The generator for the arrival process.
+	 * @param eventList The event list for the arrival process.
+	 * @param eventType The event type for the arrival process.
+	 * @param maxCustomers The maximum number of customers in the arrival process.
+	 */
 	public ArrivalProcess(ContinuousGenerator generator, EventList eventList, EventType eventType, int maxCustomers) {
 		this.generator = generator;
 		this.eventList = eventList;
@@ -17,6 +28,9 @@ public class ArrivalProcess {
 		this.maxCustomers = maxCustomers;
 	}
 
+	/**
+	 * Generates the next event in the arrival process.
+	 */
 	public void generateNext() {
 		if (customerCount < maxCustomers) {
 			eventList.add(new Event(eventType, generator.sample()));
--- src/main/java/simu/framework/Clock.java
+++ src/main/java/simu/framework/Clock.java
@@ -1,13 +1,27 @@
 package simu.framework;
 
+/**
+ * This class represents a clock in the simulation.
+ * It is a singleton class, meaning there can only be one instance of this class in the application.
+ * It provides methods to get the instance of the clock, set the time, and get the time.
+ */
 public class Clock {
 	private double time;
 	private static Clock instance;
 
+	/**
+	 * Private constructor to prevent instantiation from outside the class.
+	 * Initializes the time to 0.
+	 */
 	private Clock(){
 		time = 0;
 	}
 
+	/**
+	 * Returns the instance of the Clock.
+	 * If the instance does not exist, it is created.
+	 * @return The instance of the Clock.
+	 */
 	public static Clock getInstance(){
 		if (instance == null){
 			instance = new Clock();
@@ -15,10 +29,18 @@ public class Clock {
 		return instance;
 	}
 
+	/**
+	 * Sets the time of the Clock.
+	 * @param time The time to set.
+	 */
 	public void setTime(double time){
 		this.time = time;
 	}
 
+	/**
+	 * Returns the time of the Clock.
+	 * @return The time of the Clock.
+	 */
 	public double getTime(){
 		return time;
 	}
